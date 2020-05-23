23ad9fc1c52a9a82b7d229dc5bd3795d482014fc
ChrisDirkis <c.dirkis@live.com.au>
2020-05-23 20:30:14 +1000

    first

---
+++ precache-manifest.308f0ccc834cc7c23aa56e84c3c966ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6ccd3de85c491982cb7c285ad42e75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8b8e1b2a9c172cd088d",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c505b2a46db3490fb69a",
+    "url": "/static/js/2.c1f2a9f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c1f2a9f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b8e1b2a9c172cd088d",
+    "url": "/static/js/main.231a4878.chunk.js"
+  },
+  {
+    "revision": "3b061b9da2abcfd19e82",
+    "url": "/static/js/runtime-main.53ec5846.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
